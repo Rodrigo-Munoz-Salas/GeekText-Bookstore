e2eed4e82c7103b32c43793bccbdef18b3495085
Rodrigo-Munoz-Salas <rodrigomunoz2003@gmail.com>
2025-02-26 15:23:33 -0500

    Added functionality for an user to create their wishlist, given the id

---
+++ handler_wishlist.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/Rodrigo-Munoz-Salas/GeekText-Bookstore/internal/database"
+	"github.com/google/uuid"
+)
+
+// creates a new wishlist for the given user_id
+func (apiCgf *apiConfig) handlerCreateWishlist(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		UserID   uuid.UUID `json:"user_id"`
+		ListName string    `json:"list_name"`
+	}
+	decoder := json.NewDecoder(r.Body)
+
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error parsing JSON: %v", err))
+		return
+	}
+
+	// Checking the number of wishlists that the user has
+	count_wishlists, err := apiCgf.DB.CountUserWishlists(r.Context(), params.UserID)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Failed to check wishlist count for '%v'", params.UserID))
+		return
+	}
+	// Do not create wishlist if the user has previously created 3 wishlists.
+	if count_wishlists >= 3 {
+		respondWithError(w, 400, fmt.Sprintf("The user with id '%v' already has 3 wishlists", params.UserID))
+		return
+	}
+
+	// Create the wishlist if no errors occur
+	wishlist, err := apiCgf.DB.CreateWishlist(r.Context(), database.CreateWishlistParams{
+		ID:       uuid.New(),
+		UserID:   params.UserID,
+		ListName: params.ListName,
+	})
+
+	if err != nil {
+		// Check the user with the given id exists in the database
+		if strings.Contains(err.Error(), "foreign key constraint") {
+			respondWithError(w, 400, fmt.Sprintf("User with id '%v' does not exist", params.UserID))
+		}
+		// Check the names of each wishlist is unique for the same user id
+		if strings.Contains(err.Error(), "no rows in result set") {
+			respondWithError(w, 400, "Coudln't create the wishlist: Duplicate names are not allowed")
+		}
+		return
+	}
+
+	responseWithJSON(w, 201, databaseWishlistToWishlist(wishlist))
+}
--- models.go
+++ models.go
@@ -26,3 +26,17 @@ func databaseUserToUser(dbUser database.User) User {
 		HomeAddress:  dbUser.HomeAddress,
 	}
 }
+
+type Wishlist struct {
+	ID       uuid.UUID `json:"id"`
+	UserID   uuid.UUID `json:"user_id"`
+	ListName string    `json:"list_name"`
+}
+
+func databaseWishlistToWishlist(dbWishlist database.Wishlist) Wishlist {
+	return Wishlist{
+		ID:       dbWishlist.ID,
+		UserID:   dbWishlist.UserID,
+		ListName: dbWishlist.ListName,
+	}
+}
