98753c2ed4c0613c3284eac676e7d14a929c9cd9
Pierson <pmand010@fiu.edu>
2025-03-17 20:30:22 -0400

    Implementation of 3 items for feature

--- sql/queries/books.sql
+++ sql/queries/books.sql
@@ -1,10 +1,10 @@
 -- name: GetBooksByGenre :many
-SELECT id, title, description, price::FLOAT8 AS price, genre, publisher_id, year_published 
+SELECT id, title, description, price, genre, publisher_id, year_published 
 FROM books 
 WHERE genre = $1;
 
 -- name: GetTopSellingBooks :many
-SELECT b.id, b.title, b.description, b.price::FLOAT8 AS price, b.genre, b.publisher_id, b.year_published 
+SELECT b.id, b.title, b.description, b.price, b.genre, b.publisher_id, b.year_published 
 FROM books b
 JOIN book_authors ba ON b.id = ba.book_id
 JOIN authors a ON ba.author_id = a.id
@@ -12,7 +12,7 @@ ORDER BY b.year_published DESC
 LIMIT 10;
 
 -- name: GetBooksByRating :many
-SELECT b.id, b.title, b.description, b.price::FLOAT8 AS price, b.genre, b.publisher_id, b.year_published, AVG(r.rating) AS average_rating
+SELECT b.id, b.title, b.description, b.price, b.genre, b.publisher_id, b.year_published, AVG(r.rating) AS average_rating
 FROM books b
 JOIN ratings r ON b.id = r.book_id
 GROUP BY b.id
@@ -21,4 +21,4 @@ HAVING AVG(r.rating) >= $1;
 -- name: ApplyDiscountToPublisher :exec
 UPDATE books 
 SET price = price * (1 - sqlc.arg(discount_percent)::FLOAT8) 
-WHERE publisher_id = sqlc.arg(publisher_id);
+WHERE publisher_id = sqlc.arg(publisher_id)::UUID;
